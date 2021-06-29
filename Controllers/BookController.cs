1f4f778e8429b531f7190b57f75e8d51b60cb9ca
DELL <DELL@DESKTOP-15VLH02>
2021-06-29 22:58:47 +0700

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lap3.Models;
+namespace Lap3.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public string HelloTeacher(string university)
+        {
+            return "Hello Nguyen Thi Ngoc Chau - University" + university;
+        }
+        public ActionResult ListBook(string unniversity)
+        {
+            List<string> listBook = new List<string>();
+            listBook.Add("HTML % CSSS the complete Manual-Author Name Book 1");
+            listBook.Add("HTML % CSSS the complete Manual-Author Name Book 2");
+            listBook.Add("HTML % CSSS the complete Manual-Author Name Book 3");
+            ViewBag.Books = listBook;
+            return View();
+        }
+        public ActionResult ListBookMode()
+        { 
+            List<Book> listBook = new List<Book>();
+            Book book1 = new Book(1, "Sach 1", "Tac gia 1", "/Content/Images/hinhanh1.jpg");
+            listBook.Add(book1);
+            listBook.Add(new Book(10,"sach 10","Tac gia 10", "/Content/Images/hinhanh10.jpg"));
+            listBook.Add(new Book (100, "sach 100", "Tac gia 100", "/Content/Images/hinhanh100.jpg"));
+            ViewBag.Books = listBook;
+            return View();
+        }
+
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lap3.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+        private int v1;
+        private string v2;
+        private string v3;
+
+        public Book()
+        {
+        }
+
+        public Book(int v1, string v2, string v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+        public Book(int id, string title, string author, string image_cover)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = image_cover;
+        }
+        public int Id
+        {
+            get { return id; }
+        }
+        public string Title
+        {
+            get { return title; }
+        }
+        public string Author
+        {
+            get { return author; }
+        }
+        public string ImageCover
+        {
+            get { return image_cover; }
+        }
+    }
+}
